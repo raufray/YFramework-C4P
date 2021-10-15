291ca477e67090e70f3a14fb3ce5c1511a6bf245
raufmardiyev <raufmardiyev@gmail.com>
2021-10-15 01:46:34 -0500

    TestPage added

--- src/test/java/runners/UITestRunner.java
+++ src/test/java/runners/UITestRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
         "json:target/cucumber.json"},
         features="src/test/resources/uiFeatures",
         glue="ui_automation.step_definitions",
-        tags="@run",
+        tags="@Parallel",
         dryRun = false,
         monochrome = true
 )
---
+++ src/test/java/ui_automation/pages/TestPage.java
@@ -0,0 +1,12 @@
+package ui_automation.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TestPage {
+
+
+    @FindBy(id = "input")
+    WebElement inputBtn;
+
+}
